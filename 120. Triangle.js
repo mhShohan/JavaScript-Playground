f5f7b8ccbd65d4e645913550432b8182d7d1f259
mhshohan1 <mehdihasanshohan25@gmail.com>
2022-06-13 20:21:55 +0600

    120. Triangle

---
+++ 120. Triangle.js
@@ -0,0 +1,14 @@
+var minimumTotal = function (triangle) {
+    let result = 0
+
+    for (let i of triangle) {
+        result += Math.min(...i)
+        console.log(Math.min(...i));
+    }
+
+    return result
+};
+
+// console.log(minimumTotal([[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]));
+// console.log(minimumTotal([[-10]]));
+console.log(minimumTotal([[-1], [2, 3], [1, -1, -3]]));
