21b00fbd5df7675e2ca0d2801bc702ebff7f89e6
mhshohan1 <mehdihasanshohan25@gmail.com>
2023-03-11 00:17:56 +0600

    abstruction

---
+++ oop/abstruction.js
@@ -0,0 +1,4 @@
+/* Abstraction in programming refers to the concept of hiding complex
+implementation details and exposing only the necessary information or
+functionalities to the user. In JavaScript, we can achieve abstraction
+using objects and classes. */
