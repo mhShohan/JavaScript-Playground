dd2604b801b43e98c4e9b90c7d1ac30b4d1a145e
mhshohan1 <mehdihasanshohan25@gmail.com>
2023-03-25 16:45:33 +0600

    Built-in and Custom  Data Structures

---
+++ Data Structure and Algorithm/Codevolution/36.DS-intro.js
@@ -0,0 +1,23 @@
+/* 
+    Data structure is a data organization, management, and storage format 
+    that is usually chosen for efficient access to data.
+*/
+
+/**
+ * Built in Data Structures
+ *   => Array
+ *   => Object
+ *   => Sets
+ *   => Maps
+ */
+
+/**
+ * Custom Data Structures
+ *   => Stack 
+ *   => Queues
+ *   => Circular Queues
+ *   => Linked List 
+ *   => Hash Table 
+ *   => Trees
+ *   => Graphs
+ */
