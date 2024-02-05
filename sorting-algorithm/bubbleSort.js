16c1fcd0427e735986a18551d550aa53b3bacce3
mhshohan1 <mehdihasanshohan25@gmail.com>
2024-02-05 22:34:18 +0600

    bubble sort algorithm

---
+++ sorting-algorithm/bubbleSort.js
@@ -0,0 +1,17 @@
+const numArr = [4, 2, 5, 7, 8, 2, 1, 9, 0];
+
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+};
+
+console.log(bubbleSort(numArr));
